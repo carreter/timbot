b4932f1808afc7ea46bccfd910c2fc68b447cd03
palmerito0 <sandiegobutterflies@gmail.com>
2019-12-15 16:29:22 -0800

    Update commands so they work with new config system

--- commands.py
+++ commands.py
@@ -15,7 +15,7 @@ class PingCommand(BotCommand):
 
         super().__init__(name, command_help)
 
-    async def _command_function(self, ctx):
+    async def _command_function(self, config, ctx):
         await ctx.channel.send('Pong!')
 
 class RolesCommand(BotCommand):
@@ -24,7 +24,7 @@ class RolesCommand(BotCommand):
 
     TODO: Implement ability to change self-assign status of roles on the fly.
     """
-    def __init__(self, self_assignable_roles: List[str]):
+    def __init__(self):
         """
         Create RolesCommand object.
 
@@ -33,71 +33,71 @@ class RolesCommand(BotCommand):
         name = 'roles'
         command_help = CommandHelp()
 
-        self._self_assignable_roles = self_assignable_roles
-
         super().__init__(name, command_help)
 
-    async def _print_roles(self, channel):
+    async def _print_roles(self, roles, channel):
         """ 
         Creates Discord-formatted representation of self-assignable roles
         and sends it in channel.
         """
         role_message = 'List of self-assignable roles: \n------------------------------\n```'
 
-        for role in self._self_assignable_roles:
+        for role in roles:
             role_message += '{}\n'.format(role)
 
         role_message += '```'
 
         await channel.send(role_message)
 
-    async def _self_assign_role(self, role_name: str, member: discord.Member, channel: discord.TextChannel):
+    async def _assign_role(self, role_name: str, member: discord.Member, channel: discord.TextChannel):
         """
-        Assigns role to member if role is in _self_assignable_roles.
+        Assigns role to member.
         Prints success/failure in channel.
         """
         # TODO: check if user has/doesn't have role in the first place
-        if role_name in self._self_assignable_roles: # Check that a role is listed as assignable
-            role = discord.utils.get(channel.guild.roles, name = role_name) # Get the role object
-            await member.add_roles(role) # Assign the role
+        role = discord.utils.get(channel.guild.roles, name = role_name) # Get the role object
+        await member.add_roles(role) # Assign the role
 
-            await asyncio.sleep(.5) # Wait before checking that role was successfully added
-            if role in member.roles: # Verify that role is assigned
-                await channel.send('Role `{}` successfully assigned!'.format(role_name))
-            else:
-                await channel.send('Role `{}` could not be assigned. Try again.'.format(role_name))
+        await asyncio.sleep(.5) # Wait before checking that role was successfully added
+        if role in member.roles: # Verify that role is assigned
+            await channel.send('Role `{}` successfully assigned!'.format(role_name))
         else:
-            await channel.send('`{}` is not a self-assignable role!'.format(role_name))
+            await channel.send('Role `{}` could not be assigned. Try again.'.format(role_name))
 
-    async def _self_remove_role(self, role_name: str, member: discord.Member, channel: discord.TextChannel):
+    async def _remove_role(self, role_name: str, member: discord.Member, channel: discord.TextChannel):
         """
-        Removes role from member if role is in _self_assignable_roles.
+        Removes role from member.
         Prints success/failure in channel.
         """
         # TODO: check if user has/doesn't have role in the first place
-        if role_name in self._self_assignable_roles: # Check that a role is listed as assignable
-            role = discord.utils.get(channel.guild.roles, name = role_name) # Get the role object
-            await member.remove_roles(role) # Assign the role
+        role = discord.utils.get(channel.guild.roles, name = role_name) # Get the role object
+        await member.remove_roles(role) # Assign the role
 
-            await asyncio.sleep(.5) # Wait before checking that role was successfully removed
-            if not role in member.roles: # Verify that role is removed
-                await channel.send('Role `{}` successfully removed!'.format(role_name))
-            else:
-                await channel.send('Role `{}` could not be removed. Try again.'.format(role_name))
+        await asyncio.sleep(.5) # Wait before checking that role was successfully removed
+        if not role in member.roles: # Verify that role is removed
+            await channel.send('Role `{}` successfully removed!'.format(role_name))
         else:
-            await channel.send('`{}` is not a self-assignable role!'.format(role_name))
+            await channel.send('Role `{}` could not be removed. Try again.'.format(role_name))
 
-    async def _command_function(self, ctx: Context):
+    async def _command_function(self, config: dict, ctx: Context):
         if not ctx.args:
             return await ctx.channel.send('Roles command must be run with argument `list`, `iam`, or `iamnot`.')
 
         sub_command = ctx.args[0]
         role_name = ' '.join(ctx.args[1:])
+        self_assignable_roles = config[self.name]['self_assignable_roles']
+
         if sub_command == 'list':
-            await self._print_roles(ctx.channel)
+            await self._print_roles(self_assignable_roles, ctx.channel)
         elif sub_command == 'iam':
-            await self._self_assign_role(role_name, ctx.author, ctx.channel)
+            if role_name in self_assignable_roles: # Check that a role is listed as assignable
+                await self._assign_role(role_name, ctx.author, ctx.channel)
+            else:
+                await ctx.channel.send('`{}` is not a self-assignable role!'.format(role_name))
         elif sub_command == 'iamnot':
-            await self._self_remove_role(role_name, ctx.author, ctx.channel)
+            if role_name in self_assignable_roles: # Check that a role is listed as assignable
+                await self._remove_role(role_name, ctx.author, ctx.channel)
+            else:
+                await ctx.channel.send('`{}` is not a self-assignable role!'.format(role_name))
         else:
             await ctx.channel.send('Unrecognized roles command `{}`. Please try `list`, `iam`, or `iamnot`.'.format(sub_command))
